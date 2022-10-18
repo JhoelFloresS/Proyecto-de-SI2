01ac9069e8dbcd8d0d06b3c5f825bde437d69199
Andres Silva <andreschilva3@gmail.com>
2022-10-18 17:57:25 -0400

    creando migraciones base datos de inquilinos

---
+++ database/migrations/tenant/2022_10_18_213103_reate_bitacoras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacoras', function (Blueprint $table) {
+            $table->id();
+            $table->string('accion');
+            $table->dateTime('fecha');
+            $table->string('ip_maquina');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->on('users')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacoras');
+    }
+};
